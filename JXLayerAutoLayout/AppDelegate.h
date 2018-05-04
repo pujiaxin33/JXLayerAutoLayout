538ed595069b562c20824417f57c21e523f03321
pjx <pujx@jingbo360.com>
2018-05-04 17:47:16 +0800

    Initial Commit

---
+++ JXLayerAutoLayout/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JXLayerAutoLayout
+//
+//  Created by jiaxin on 2018/5/4.
+//  Copyright © 2018年 jiaxin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JXLayerAutoLayout/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JXLayerAutoLayout
+//
+//  Created by jiaxin on 2018/5/4.
+//  Copyright © 2018年 jiaxin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
